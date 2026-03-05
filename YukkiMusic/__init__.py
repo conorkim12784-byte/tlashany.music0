8658a9605235281c8507627938f5d58a381b5470
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-05 11:20:38 +0200

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -7,57 +7,41 @@
 #
 # All rights reserved.
 
-import os
-import sys
-from typing import List
+from YukkiMusic.core.bot import YukkiBot
+from YukkiMusic.core.dir import dirr
+from YukkiMusic.core.git import git
+from YukkiMusic.core.userbot import Userbot
+from YukkiMusic.misc import dbb, heroku, sudo
 
-import yaml
+from .logging import LOGGER
 
-languages = {}
-commands = {}
+# Directories
+dirr()
 
+# Check Git Updates
+git()
 
-languages_present = {}
+# Initialize Memory DB
+dbb()
 
+# Heroku APP
+heroku()
 
-def get_command(value: str) -> List:
-    return commands["command"][value]
+# Load Sudo Users from DB
+sudo()
 
+# Bot Client
+app = YukkiBot()
 
-def get_string(lang: str):
-    return languages[lang]
+# Assistant Client
+userbot = Userbot()
 
+from .platforms import *
 
-for filename in os.listdir(r"./strings"):
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
-
-
-for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name][
-            "name"
-        ]
-    except:
-        print(
-            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
-        )
-        pass
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()

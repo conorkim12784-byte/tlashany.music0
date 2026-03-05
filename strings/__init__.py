dcfc90a5a7146fbf8772a69225d6c4477f69f215
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-05 11:23:11 +0200

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -60,4 +60,4 @@ for filename in os.listdir(r"./strings/langs/"):
         print(
             "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
         )
-        sys.exit()
+        pass

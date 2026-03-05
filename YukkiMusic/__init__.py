78bca2b6e20f76255027070227fe6bc52d8c5cb2
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-05 11:15:06 +0200

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -7,41 +7,57 @@
 #
 # All rights reserved.
 
-from YukkiMusic.core.bot import YukkiBot
-from YukkiMusic.core.dir import dirr
-from YukkiMusic.core.git import git
-from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku, sudo
+import os
+import sys
+from typing import List
 
-from .logging import LOGGER
+import yaml
 
-# Directories
-dirr()
+languages = {}
+commands = {}
 
-# Check Git Updates
-git()
 
-# Initialize Memory DB
-dbb()
+languages_present = {}
 
-# Heroku APP
-heroku()
 
-# Load Sudo Users from DB
-sudo()
+def get_command(value: str) -> List:
+    return commands["command"][value]
 
-# Bot Client
-app = YukkiBot()
 
-# Assistant Client
-userbot = Userbot()
+def get_string(lang: str):
+    return languages[lang]
 
-from .platforms import *
 
-YouTube = YouTubeAPI()
-Carbon = CarbonAPI()
-Spotify = SpotifyAPI()
-Apple = AppleAPI()
-Resso = RessoAPI()
-SoundCloud = SoundAPI()
-Telegram = TeleAPI()
+for filename in os.listdir(r"./strings"):
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        commands[language_name] = yaml.safe_load(
+            open(r"./strings/" + filename, encoding="utf8")
+        )
+
+
+for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
+    if filename.endswith(".yml"):
+        language_name = filename[:-4]
+        if language_name == "en":
+            continue
+        languages[language_name] = yaml.safe_load(
+            open(r"./strings/langs/" + filename, encoding="utf8")
+        )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name][
+            "name"
+        ]
+    except:
+        print(
+            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
+        )
+        pass
